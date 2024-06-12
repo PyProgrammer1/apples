ff0521be275f551198345f3cbd968f3f991b4a65
PyProgrammer1 <154099800+PyProgrammer1@users.noreply.github.com>
2024-06-12 11:39:34 -0600

    Fixed with shellcheck recommendations

--- game.sh
+++ game.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo -e "Welcome to Apples!\nApples! is a game where you are born on a farm and are trying to make money by selling apples."
-read -p "Ready to start? [yes/no] "
-if $REPLY == "yes"
+read -rp "Ready to start? [yes/no] "
+if $REPLY = "yes"
 then
-    echo "Starting..."
+    echo "Starting"
 else
-    break
+    exit
 fi
