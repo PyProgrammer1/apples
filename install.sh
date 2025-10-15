d82722fd0f5326b63f52a52a246a63739346f0f2
PyProgrammer1 <154099800+PyProgrammer1@users.noreply.github.com>
2025-10-15 11:30:01 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 wget https://raw.githubusercontent.com/PyProgrammer1/apples/
 cd apples || exit
 chmod -x game.sh
-echo "Run cd apples && bash game.sh to run from command line, or use the desktop entry."
+echo "Run cd apples && ./game.sh to run from command line, or use the desktop entry."
